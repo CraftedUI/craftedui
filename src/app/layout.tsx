fea0707aaef450dc3537218d3de0fc269be3391d
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-06-03 21:43:34 +0530

    🪄 craftedui init

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Crafted UI",
+  description: "Handpicked set of UI components, boosts your app.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
