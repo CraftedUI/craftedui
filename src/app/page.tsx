cd4788dadeb6697cc03c2241ef21e96118ec08bb
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-06-03 22:42:06 +0530

    added favicons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
       
-
+      <Image
+          className=""
+          src="/logo.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
     </main>
   );
 }
